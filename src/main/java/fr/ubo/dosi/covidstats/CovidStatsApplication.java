6f749eec0e411642e91bf604e3703104d5c0d83b
AKKAR Ferdaous <46265098+ferdaousak@users.noreply.github.com>
2022-02-01 15:59:39 +0100

    add swagger enable

--- src/main/java/fr/ubo/dosi/covidstats/CovidStatsApplication.java
+++ src/main/java/fr/ubo/dosi/covidstats/CovidStatsApplication.java
@@ -3,7 +3,10 @@ package fr.ubo.dosi.covidstats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CovidStatsApplication {
 
 	public static void main(String[] args) {
