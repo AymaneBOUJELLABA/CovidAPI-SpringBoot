4ba1c1825ef351afafd2ab5d6564d47e849faecd
AymaneBOUJELLABA <aymane.boujellaba@gmail.com>
2022-02-01 14:24:01 +0100

    Update PaysCSVDB.java

--- src/main/java/fr/ubo/dosi/covidstats/db/PaysCSVDB.java
+++ src/main/java/fr/ubo/dosi/covidstats/db/PaysCSVDB.java
@@ -1,5 +1,20 @@
 package fr.ubo.dosi.covidstats.db;
 
-public class PaysCSVDB {
+public class PaysCSVDB
+{
+	private static PaysCSVDB instance = null;
+	
+	private PaysCSVDB()
+	{
+		
+	}
+	
+	public static PaysCSVDB getInstance()
+	{
+		if(instance == null)
+			instance = new PaysCSVDB();
+		
+		return instance;
+	}
 
 }
