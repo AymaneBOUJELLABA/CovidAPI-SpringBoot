7713889f4616ff4a64e08d3fb6de52210b36d62c
AymaneBOUJELLABA <aymane.boujellaba@gmail.com>
2022-02-01 14:35:25 +0100

    Update PaysCSVDB.java

--- src/main/java/fr/ubo/dosi/covidstats/db/PaysCSVDB.java
+++ src/main/java/fr/ubo/dosi/covidstats/db/PaysCSVDB.java
@@ -1,5 +1,15 @@
 package fr.ubo.dosi.covidstats.db;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+
+import ch.qos.logback.classic.Logger;
+
 public class PaysCSVDB
 {
 	private static PaysCSVDB instance = null;
@@ -15,6 +25,23 @@ public class PaysCSVDB
 			instance = new PaysCSVDB();
 		
 		return instance;
+	}
+	
+	public <T> List<T> loadObjectList(Class<T> type, String fileName)
+	{
+	    try
+	    {
+	        CsvSchema bootstrapSchema = CsvSchema.emptySchema().withHeader();
+	        CsvMapper mapper = new CsvMapper();
+	        File file = new ClassPathResource(fileName).getFile();
+	        MappingIterator<T> readValues = 
+	          mapper.reader(type).with(bootstrapSchema).readValues(file);
+	        return readValues.readAll();
+	    } catch (Exception e)
+	    {
+	        System.err.println("Error occurred while loading object list from file " + fileName + e);
+	        return Collections.emptyList();
+	    }
 	}
 
 }
